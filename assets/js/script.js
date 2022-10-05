fd47be4de69481964769857213f0f55dc7e71793
Maxim <sokolov2710@mail.ru>
2022-10-05 18:21:57 +0300

    1.0

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+(function sliderFunc() {
+  // Объявление слайдера для фото товаров
+  var swiper = new Swiper(".swiper-container", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 0,
+    effect: "fade",
+    preventClicks: false,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  let products = document.querySelectorAll(".product");
+  let paginationButtons = document.querySelectorAll(
+    ".swiper-pagination-bullet"
+  );
+
+  // Переключение слайдов за счёт движения курсора
+  paginationButtons.forEach((element) =>
+    element.addEventListener("mouseover", (event) => {
+      event.stopPropagation();
+      event.target.click();
+    })
+  );
+
+  // Возвращение на 1-й слайд, когда курсор не на карточке товара
+  products.forEach((element) => {
+    element.addEventListener("mouseleave", () => {
+      paginationButtons.forEach((pagButton) => {
+        if (pagButton.getAttribute("aria-label") == "Go to slide 1") {
+          pagButton.click();
+        }
+      });
+    });
+    // Кликабельность ссылки товара
+    element.addEventListener("mouseup", (event) => {
+      if (event.which == 1) {
+        window.location = element.href;
+      }
+    });
+  });
+})();
